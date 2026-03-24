510c868bc4488f9800c6aa32c523eff6342d1ab3
velazquezgreg <velazquezgreg@ca.frre.utn.edu.ar>
2026-03-24 01:32:58 -0300

    primera pagina torneo test

---
+++ app/torneo/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TorneoPage() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Torneo Test</h1>
+      <p>Formulario de inscripción próximamente...</p>
+    </main>
+  );
+}
